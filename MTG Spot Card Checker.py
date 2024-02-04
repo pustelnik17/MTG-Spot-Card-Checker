eac90146987d7f1b473de102fc61c5252dca26f9
FoxFate <solembum.pospolity@gmail.com>
2024-02-05 00:33:21 +0100

    Add GUI application

--- MTG Spot Card Checker.py
+++ MTG Spot Card Checker.py
@@ -1,4 +1,60 @@
-import CardManager as cm
+import CardManager
+import customtkinter as gui
+from PIL import Image
+import re
 
-data = cm.Data()
-print(data.result)
+def Load():
+    global text
+    with open("data.txt", mode="r") as file:
+        for element in file.readlines():
+            inputText.insert('end', element)
+
+def Search():
+    result = inputText.get(0.0, "end").splitlines()
+    for cardId in range(len(result)):
+        result[cardId] = re.sub("^[0-9]* ", "", result[cardId])
+    search = CardManager.Data(result)
+    sum = count = 0
+    outputText.delete(0.0, "end")
+    for card in search.result:
+        count += 1
+        sum += float(card["cost"])
+        outputText.insert("end", "[" + str( card["cost"]) + "] " + card["name"] + "\n")
+    summaryLabel.configure(text=f"{count} of {len(result)} | {round(sum, 2)} zł")
+    myTab.set("Output")
+
+def Save():
+    with open("data.txt", mode="w") as file:
+        file.write(inputText.get(0.0, "end"))
+
+gui.set_appearance_mode("dark")
+gui.set_default_color_theme("dark-blue")
+
+root = gui.CTk()
+root.geometry("400x1000+0+0")
+root.title("MTG Spot Card Checker")
+
+searchImage = gui.CTkImage(Image.open("Resources/search.png").resize((20, 20)))
+saveImage = gui.CTkImage(Image.open("Resources/save.png").resize((20, 20)))
+
+
+buttonFrame = gui.CTkFrame(root)
+searchButton = gui.CTkButton(buttonFrame, image=searchImage, text="", width=90, command=Search)
+saveButton = gui.CTkButton(buttonFrame, image=saveImage, text="", width=90, command=Save)
+myTab = gui.CTkTabview(root)
+inputTab = myTab.add("Input")
+outputTab = myTab.add("Output")
+inputText = gui.CTkTextbox(inputTab, font=("consolas", 14))
+outputText = gui.CTkTextbox(outputTab, font=("consolas", 14))
+summaryLabel = gui.CTkLabel(outputTab, text="Summary", font=("consolas", 18))
+
+Load()
+buttonFrame.pack(pady=10, padx=40)
+searchButton.grid(row=0, column=0, padx=10)
+saveButton.grid(row=0, column=1, padx=10)
+myTab.pack(fill="both", expand=True)
+inputText.pack(pady=10, padx=40, fill="both", expand=True)
+summaryLabel.pack(pady=10, padx=40, fill="x")
+outputText.pack(pady=10, padx=40, fill="both", expand=True)
+
+root.mainloop()
