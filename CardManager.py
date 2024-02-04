8c46bce5f6494094c4c5fe76a8c4f2741c32ed4e
FoxFate <solembum.pospolity@gmail.com>
2024-02-05 00:31:47 +0100

    Delete get data function

--- CardManager.py
+++ CardManager.py
@@ -5,8 +5,8 @@ import asyncio
 import json
 
 class Data:
-    def __init__(self, inputFilePath="data.txt", offerLimit=50):
-        self.cards = Data.GetCards(inputFilePath)
+    def __init__(self, cards, offerLimit=50):
+        self.cards = cards
         self.cardsJSON = asyncio.run(Data.Fetch(self, offerLimit))
         self.result = Data.GetMinimalCostOffers(self)
 
@@ -25,12 +25,4 @@ class Data:
                 if card["data"][offerId]["stock"] > 0 and card["data"][offerId]["rarity"] != "Tip Card":
                     prices.append(card["data"][offerId]["price"])
             result.append(min(prices) if len(prices) > 0 else -1)
-        return [{"names": self.cards[i], "cost": result[i]} for i in range(len(self.cards))]
-
-    @staticmethod
-    def GetCards(path):
-        with open(path) as file:
-            result = [element.strip() for element in file.readlines()]
-            for cardId in range(len(result)):
-                result[cardId] = re.sub("^[0-9]* ", "", result[cardId])
-            return result
+        return [{"name": self.cards[i], "cost": result[i]} for i in range(len(self.cards))]
