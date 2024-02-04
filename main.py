47c3c7f939a8502725db785cdd14e22aad5fbda7
FoxFate <solembum.pospolity@gmail.com>
2024-02-04 21:21:43 +0100

    Add async request manager

--- main.py
+++ main.py
@@ -1,28 +1,43 @@
 from urllib.parse import quote
-from urllib3 import PoolManager
+import re
+import httpx
+import asyncio
+import json
 
-def GetCards(path):
-    with open(path) as file:
-        return [element.strip() for element in file.readlines()]
-def GetJSON(name, http):
-    response = http.request("GET", "https://mtgspot.cn-panel.pl/products?s_title=" + quote(name) + "&limit=25&offset=0&order_by=price&sort_by=asc")
-    return response.json()
-def InStock(item):
-    if item["rarity"] != "Tip Card" and item["stock"] > 0:
-        return True
-    return False
+class Data:
+    def __init__(self, inputFilePath="data.txt", offerLimit=50):
+        self.cards = Data.GetCards(inputFilePath)
+        self.cardsJSON = asyncio.run(Data.Fetch(self, offerLimit))
+        self.result = Data.GetMinimalCostOffers(self)
+
+    async def Fetch(self, limit):
+        timeout = httpx.Timeout(10.0, read=None)
+        async with httpx.AsyncClient() as client:
+            reqs = [client.get("https://mtgspot.cn-panel.pl/products?s_title=" + quote(card) + f"&limit={limit}&offset=0&order_by=price&sort_by=asc", timeout=timeout) for card in self.cards]
+            result = await asyncio.gather(*reqs)
+        return [json.loads(element.text) for element in result]
+
+    def GetMinimalCostOffers(self):
+        result = []
+        for card in self.cardsJSON:
+            prices = []
+            for offerId in range(len(card["data"])):
+                if card["data"][offerId]["stock"] > 0 and card["data"][offerId]["rarity"] != "Tip Card":
+                    prices.append(card["data"][offerId]["price"])
+            result.append(min(prices) if len(prices) > 0 else -1)
+        return [{"names": self.cards[i], "cost": result[i]} for i in range(len(self.cards))]
+
+    @staticmethod
+    def GetCards(path):
+        with open(path) as file:
+            result = [element.strip() for element in file.readlines()]
+            for cardId in range(len(result)):
+                result[cardId] = re.sub("^[0-9]* ", "", result[cardId])
+            return result
+
 def main():
-    http = PoolManager()
-    cardList = GetCards("data.txt")
-    result = dict.fromkeys(cardList)
-    offer = [GetJSON(card, http) for card in cardList]
-    for cardIterator in offer:
-        for item in cardIterator["data"]:
-            if InStock(item):
-                result[item["title"]] = item["price"]
-                break
-    for element in result:
-        print(element, " ---> ", result[element])
+    data = Data(inputFilePath="data.txt")
+    print(data.result)
 
 
 if __name__ == "__main__":
