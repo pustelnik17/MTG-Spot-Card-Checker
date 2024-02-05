a377fb651652174b130ac8d0f56a698501aecb67
FoxFate <solembum.pospolity@gmail.com>
2024-02-05 02:15:25 +0100

    Add Maximum price per card feature

--- CardManager.py
+++ CardManager.py
@@ -5,8 +5,9 @@ import asyncio
 import json
 
 class Data:
-    def __init__(self, cards, offerLimit=50):
+    def __init__(self, cards, upperBound=None, offerLimit=50):
         self.cards = cards
+        self.upperBound = upperBound
         self.cardsJSON = asyncio.run(Data.Fetch(self, offerLimit))
         self.result = Data.GetMinimalCostOffers(self)
 
@@ -22,7 +23,7 @@ class Data:
         for card in self.cardsJSON:
             prices = []
             for offerId in range(len(card["data"])):
-                if card["data"][offerId]["stock"] > 0 and card["data"][offerId]["rarity"] != "Tip Card":
+                if card["data"][offerId]["stock"] > 0 and card["data"][offerId]["rarity"] != "Tip Card" and float(card["data"][offerId]["price"]) <= self.upperBound:
                     prices.append(card["data"][offerId]["price"])
             result.append(min(prices) if len(prices) > 0 else -1)
         return [{"name": self.cards[i], "cost": result[i]} for i in range(len(self.cards))]
--- MTGSpotCardChecker.py
+++ MTGSpotCardChecker.py
@@ -8,18 +8,22 @@ def Load():
     with open("data.txt", mode="r") as file:
         for element in file.readlines():
             inputText.insert('end', element)
+        priceText.insert('end', "50")
 
 def Search():
     result = inputText.get(0.0, "end").splitlines()
     for cardId in range(len(result)):
         result[cardId] = re.sub("^[0-9]* ", "", result[cardId])
-    search = CardManager.Data(result)
+    search = CardManager.Data(result, upperBound=float(priceText.get(0.0, "end")))
     sum = count = 0
     outputText.delete(0.0, "end")
     for card in search.result:
-        count += 1
-        sum += float(card["cost"])
-        outputText.insert("end", "[" + str( card["cost"]) + "] " + card["name"] + "\n")
+        if card["cost"] != -1:
+            count += 1
+            sum += float(card["cost"])
+            outputText.insert("end", "[" + str(card["cost"]) + "] " + ((card["name"][:25] + '..') if len(card["name"]) > 25 else card["name"]) + "\n")
+        else:
+            outputText.insert("end", "[----] " + ((card["name"][:25] + '..') if len(card["name"]) > 25 else card["name"]) + "\n")
     summaryLabel.configure(text=f"{count} of {len(result)} | {round(sum, 2)} zł")
     myTab.set("Output")
 
@@ -45,6 +49,9 @@ myTab = gui.CTkTabview(root)
 inputTab = myTab.add("Input")
 outputTab = myTab.add("Output")
 inputText = gui.CTkTextbox(inputTab, font=("consolas", 14))
+priceFrame = gui.CTkFrame(inputTab)
+priceLabel = gui.CTkLabel(priceFrame, text="Max Per Card [zł]:", font=("consolas", 18))
+priceText = gui.CTkTextbox(priceFrame, font=("consolas", 18), height=30, width=80)
 outputText = gui.CTkTextbox(outputTab, font=("consolas", 14))
 summaryLabel = gui.CTkLabel(outputTab, text="Summary", font=("consolas", 18))
 
@@ -53,6 +60,9 @@ buttonFrame.pack(pady=10, padx=40)
 searchButton.grid(row=0, column=0, padx=10)
 saveButton.grid(row=0, column=1, padx=10)
 myTab.pack(fill="both", expand=True)
+priceFrame.pack(pady=10, padx=40)
+priceLabel.grid(row=0, column=0, padx=10)
+priceText.grid(row=0, column=1, padx=10)
 inputText.pack(pady=10, padx=40, fill="both", expand=True)
 summaryLabel.pack(pady=10, padx=40, fill="x")
 outputText.pack(pady=10, padx=40, fill="both", expand=True)
